5cceae5a9846cf212bd6a687a4bccf42ae73833e
Adalberto <adalradamo03@gmail.com>
2021-05-06 16:25:58 -0600

    Adding dependencies and general files

--- POSCHAR/Startup.cs
+++ POSCHAR/Startup.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+using POSCHAR.Data;
+using POSCHAR.Email;
+using POSCHAR.Services.POS;
 
 namespace POSCHAR
 {
@@ -23,7 +30,33 @@ namespace POSCHAR
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+            //if (env.IsDevelopment())
+            //{
+            //    services.AddDbContext<ApplicationDbContext>(options =>
+            //      options.UseSqlServer(
+            //           Configuration.GetConnectionString("DefaultConnection")));
+            //}
+            //else
+            //{
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlite(
+                Configuration.GetConnectionString("SqLiteConnection")));
+            //// }
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddTransient<IRepository, Repository>();
+
+            services.Configure<EmailOptions>(Configuration);
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
 
