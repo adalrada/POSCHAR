ff02390524851ac68ba758abc603d30979349309
Adal <44960412+adalrada@users.noreply.github.com>
2021-05-07 08:50:24 -0600

    Services manually added

---
+++ POSCHAR/Services/POS/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POSCHAR.Services.POS
+{
+
+    //CHANGE 2.1
+
+    public interface IRepository
+    {
+
+        string GeneratePONumber();
+        string GenerateGRNumber();
+        string GenerateSONumber();
+        string GenerateInvenTranNumber();
+
+    }
+}
