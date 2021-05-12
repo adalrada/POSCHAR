c92e210aeff65fd2e1b8712e4c8c258e80c42d9f
Adalberto <adalradamo03@gmail.com>
2021-05-11 22:20:01 -0600

    Adding dropdown status on purchase

--- POSCHAR/Controllers/PurchasesController.cs
+++ POSCHAR/Controllers/PurchasesController.cs
@@ -109,6 +109,10 @@ namespace POSCHAR.Controllers
                 return NotFound();
             }
             ViewData["VendorId"] = new SelectList(_context.Vendor, "Id", "Name", purchase.VendorId);
+            ViewData["StatusValue"] = new List<SelectListItem>{
+                new SelectListItem { Value = "Activo", Text = "Activo" },
+                new SelectListItem { Value = "Inactivo", Text = "Inactivo" }
+            };
             return View(purchase);
         }
 
@@ -145,6 +149,7 @@ namespace POSCHAR.Controllers
                 return RedirectToAction(nameof(Index));
             }
             ViewData["VendorId"] = new SelectList(_context.Vendor, "Id", "Name", purchase.VendorId);
+            ViewData["StatusValue"] = new SelectList(_context.Vendor, "Value", "Text", purchase.Stauts);
             return View(purchase);
         }
 
